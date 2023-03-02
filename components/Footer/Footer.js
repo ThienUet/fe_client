cd63b6c06a55317b185fa909cfcf5ae13d9bb7d9
luonghv-31 <luonghv31@gmail.com>
2023-03-02 17:12:09 +0700

    Update footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,50 +1,33 @@
 import { FontSizeOutlined } from '@ant-design/icons'
 import Link from 'next/link'
 import React from 'react'
-import Image from 'next/image'
+
 import {Form, Input, Button} from 'antd'
 import { auto } from '@popperjs/core'
+import Image from '../Image/CustomImage'
 export default function Footer() {
   return (
 
     <footer className="w-100 py-4 flex-shrink-0" >
         <div className="container py-4">
-            <div className="row gy-4 gx-5">
+            <div className="row gy-4 gx-5 footer">
                 <div className="col-lg-4 col-md-6">
                     <h5 className="h3 text-left">
-                      <Image width={90} height={65} src="/static/logo/logo1.png" alt="logo" />
+                      <div className='text-center'>
+                      <Image className="footer-image" src="/static/logo/logo1.png" alt="logo" />
+                      </div>
                     </h5>              
                     <div className='row'>
-                    <div className='text-left '>
-                    
-                    <Link style={{textDecoration: 'none'}}  href="https://goo.gl/maps/2WDjYgVeBcUKsnTs9" className='fontAdd'>
-                    <p> <i className="fa fa-map" /> Địa chỉ: 198 Mỹ Đình 2, Quận Nam Từ Liêm, Hà Nội</p> 
-                    </Link>
-                    
-                    <p className="" ><i className="fa fa-phone fontPhone" ></i>Điện thoại: 0989999888</p>
-                    <i className="fa fa-envelope fontAdd"></i> zyllow.contact@gmail.com
-                    
-                   
-                    
-                   
-                      {/* <Link className='position-relative' href="#">
-                        <Image width={30} height={30} src="/static/icons/facebook.png" alt='google' />
-                      </Link>
-                      </div>
-                      <div className='col-1'>
-                      <Link className='position-relative' href="#">
-                        <Image width={30} height={30} src="/static/icons/icon_google.png" alt='google' />
-                      </Link>
-                      </div>
-                      <div className='col-1'>
-                      <Link className='position-relative' href="#">
-                        <Image width={40} height={30} src="/static/icons/zalo.jpeg" alt='google' />
-                      </Link> */}
-                      </div>
+                      <div className='text-left '>
+                        <div className='footer-text'>                      
+                          <Link style={{textDecoration: 'none'}}  href="https://goo.gl/maps/2WDjYgVeBcUKsnTs9" className='fontAdd'>
+                            <p> <i className="fa fa-map" /> Địa chỉ: 198 Mỹ Đình 2, Quận Nam Từ Liêm, Hà Nội</p> 
+                          </Link>                 
+                           <p className="" ><i className="fa fa-phone fontPhone" ></i>Điện thoại: 0989999888</p>                  
+                            <i className="fa fa-envelope fontAdd"></i> zyllow.contact@gmail.com                   
+                        </div>
+                     </div>
                     </div>
-                    
-                   
-                    
                 </div>
                 <div className="col-lg-2 col-md-6">
                     <h5 className=" mb-3">Truy cập nhanh</h5>
@@ -55,21 +38,21 @@ export default function Footer() {
                         <li><Link href="#" className="text-decoration-none">FAQ</Link></li>
                     </ul>
                 </div>
-                <div className="col-lg-2 col-md-6">
-                    <h5 className=" mb-3">Quick links</h5>
+                <div className="col-lg-3 col-md-6">
+                    <h5 className=" mb-3 fast-search">Tìm kiếm nhanh</h5>
                     <ul className="list-unstyled text-muted">
-                        <li><Link href="#" className="text-decoration-none">Home</Link></li>
-                        <li><Link href="#" className="text-decoration-none">About</Link></li>
-                        <li><Link href="#" className="text-decoration-none">Get started</Link></li>
-                        <li><Link href="#" className="text-decoration-none">FAQ</Link></li>
+                        <li><Link href="#" className="text-decoration-none">Phòng trọ Quận Cầu Giấy</Link></li>
+                        <li><Link href="#" className="text-decoration-none">Phòng trọ Quận Cầu Giấy</Link></li>
+                        <li><Link href="#" className="text-decoration-none">Phòng trọ Quận Cầu Giấy</Link></li>
+                        <li><Link href="#" className="text-decoration-none">Phòng trọ Quận Cầu Giấy</Link></li>
                     </ul>
                 </div>
-                <div className="col-lg-4 col-md-6">
+                <div className="col-lg-3 col-md-6  ">
                   <h5 className=" mb-3">ĐĂNG KÍ NHẬN TIN</h5>
                     <Input.Group compact>     
                       <Input
                         style={{
-                        width: 'calc(100% - 150px)',
+                        width: 'calc(100% - 120px)',
                        }}
                         placeholder="Nhập email của bạn"
                       />
@@ -79,7 +62,7 @@ export default function Footer() {
                       <Link href="#" className='social'>
                         <i className="fa fa-facebook-official md" aria-hidden="true"></i>
                       </Link>
-                
+                      
                       <Link href="" className='social'>
                         <i className="fa fa-telegram" aria-hidden="true"></i>
                       </Link>
@@ -91,6 +74,9 @@ export default function Footer() {
                       </Link>
                     </div>
                </div>
+            </div>
+            <div className='copyright'>
+              <p>&copy; Copyright Duong Thai Huy, Tran Quang Huy, Vi Quoc Thien, Hoang Van Luong</p>
             </div>
         </div>
     </footer>
