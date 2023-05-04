6544236220271a4c4b222894e3bb132c9db6b6ac
ThienUet <thin102000@gmail.com>
2023-05-04 11:25:48 +0700

    UPDATE

--- src/app/Payment/DonePayment.tsx
+++ src/app/Payment/DonePayment.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Button } from 'antd';
 const DonePayment: React.FC = () => {
-  return <div>DonePayment</div>;
+  return (
+    <div className='done-payment'>
+      <div className='title'>Thanh toán thành công !</div>
+      <p>Bạn đã thanh toán thành công ! Vui lòng chọn các lựa chọn bên dưới</p>
+      <div className='action-payment-done'>
+        <Button onClick={() => window.location.replace('/')}>Về trang chủ</Button>
+        <Button onClick={() => window.location.replace('/account?profileTab=payment-history')}>
+          Xem lại lịch sử thanh toán
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default DonePayment;
--- src/app/Payment/FailPayment.tsx
+++ src/app/Payment/FailPayment.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Button } from 'antd';
 const FailPayment: React.FC = () => {
-  return <div>FailPayment</div>;
+  return (
+    <div className='done-payment'>
+      <div className='title'>Thanh toán thất bại !</div>
+      <p>Bạn đã thanh toán thất bại ! Vui lòng chọn các lựa chọn bên dưới</p>
+      <div className='action-payment-done'>
+        <Button onClick={() => window.location.replace('/')}>Về trang chủ</Button>
+        <Button onClick={() => window.location.replace('/account?profileTab=payment-history')}>
+          Xem lại lịch sử thanh toán
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default FailPayment;
